8f30d421d3d0c88952bc412afaac3c0b12a10bf9
tj <ts2914@imperial.ac.uk>
2015-06-10 20:13:36 +0100

    changing the time update loop

--- codegen/templates/test_ref_iso_elastic_tmpl.cpp
+++ codegen/templates/test_ref_iso_elastic_tmpl.cpp
@@ -1,46 +1,4 @@
-/*  Copyright (C) 2015 Imperial College London and others.
- *
- *  Please see the AUTHORS file in the main source directory for a
- *  full list of copyright holders.
- *
- *  Gerard Gorman
- *  Department of Earth Science and Engineering
- *  Imperial College London
- *
- *  g.gorman@imperial.ac.uk
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *  1. Redistributions of source code must retain the above copyright
- *  notice, this list of conditions and the following disclaimer.
- *  2. Redistributions in binary form must reproduce the above
- *  copyright notice, this list of conditions and the following
- *  disclaimer in the documentation and/or other materials provided
- *  with the distribution.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
- *  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
- *  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- *  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- *  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
- *  THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- *  SUCH DAMAGE.
- */
-
-/* Test case implements:
-
-   Graves, Robert W. "Simulating seismic wave propagation in 3D
-   elastic media using staggered-grid finite differences." Bulletin of
-   the Seismological Society of America 86.4 (1996): 1091-1106.
-
-*/
+// insert copyright information
 
 #include "opesciIO.h"
 #include "opesciHandy.h"
@@ -85,16 +43,16 @@ int main(){
   assert(vs.size()==dimx*dimy*dimz);
 
   // Read rho (density).
-  std::vector<float> rho;
-  if(opesci_read_simple_binary(rhofile.c_str(), rho)){
+  std::vector<float> _rho;
+  if(opesci_read_simple_binary(rhofile.c_str(), _rho)){
     opesci_abort("Missing input file\n");
   }
   assert(rho.size()==dimx*dimy*dimz);
-  std::vector<float> buoyancy(rho.size());
+  std::vector<float> _buoyancy(rho.size());
 
   // Calculate Lame constents.
-  std::vector<float> mu, lam;
-  opesci_calculate_lame_costants(vp, vs, rho, mu, lam);
+  std::vector<float> _mu, _lam;
+  opesci_calculate_lame_costants(vp, vs, rho, _mu, _lam);
 
   // Get sources.
   std::vector<float> coorsrc, xsrc, ysrc, zsrc;
@@ -128,10 +86,13 @@ int main(){
   int snt = xsrc.size()/nsrc;
   sdt = dt;
 
+  // time periodicity for update
+  int _tp = ${time_period}
+
   // Set up solution fields.
-  std::vector<float> u(dimx*dimy*dimz), v(dimx*dimy*dimz), w(dimx*dimy*dimz),
-    txx(dimx*dimy*dimz), tyy(dimx*dimy*dimz), tzz(dimx*dimy*dimz),
-    tyz(dimx*dimy*dimz), txz(dimx*dimy*dimz), txy(dimx*dimy*dimz);
+  std::vector<float> _u(dimx*dimy*dimz*_tp), _v(dimx*dimy*dimz*_tp), _w(dimx*dimy*dimz*_tp),
+    _txx(dimx*dimy*dimz*_tp), _tyy(dimx*dimy*dimz*_tp), _tzz(dimx*dimy*dimz*_tp),
+    _tyz(dimx*dimy*dimz*_tp), _txz(dimx*dimy*dimz*_tp), _txy(dimx*dimy*dimz*_tp);
 
   // Set up seismic sections
   std::vector<float> uss, vss, wss, pss;
@@ -145,67 +106,66 @@ int main(){
     // Initialise fields exploiting first touch.
 #pragma omp for nowait
     for(int i=0;i<dimx*dimy*dimz;i++){
-      u[i] = 0.0;
-      v[i] = 0.0;
-      w[i] = 0.0;
+      _u[i] = 0.0;
+      _v[i] = 0.0;
+      _w[i] = 0.0;
 
-      txx[i] = 0.0;
-      tyy[i] = 0.0;
-      tzz[i] = 0.0;
-      tyz[i] = 0.0;
-      txz[i] = 0.0;
-      txy[i] = 0.0;
+      _txx[i] = 0.0;
+      _tyy[i] = 0.0;
+      _tzz[i] = 0.0;
+      _tyz[i] = 0.0;
+      _txz[i] = 0.0;
+      _txy[i] = 0.0;
 
-      buoyancy[i] = 1.0/rho[i];
+      _buoyancy[i] = 1.0/_rho[i];
     }
 
-    // Start of time loop.
-    float (*Lambda)[dimy][dimx] = (float (*)[dimy][dimx]) lam.data();
-    float (*Mu)[dimy][dimx] = (float (*)[dimy][dimx]) mu.data(); // need to correct to use the effective media parameters
-    float (*Buoyancy)[dimy][dimx] = (float (*)[dimy][dimx]) buoyancy.data(); // need to correct to use the effective media parameters
+    // Start of time loop, set initial values for t=0
+    float (*lambda)[dimy][dimz] = (float (*)[dimy][dimz]) _lam.data();
+    float (*mu)[dimy][dimz] = (float (*)[dimy][dimz]) _mu.data(); // need to correct to use the effective media parameters
+    float (*beta)[dimy][dimz] = (float (*)[dimy][dimz]) _buoyancy.data(); // need to correct to use the effective media parameters
 
-    float (*U)[dimy][dimx] = (float (*)[dimy][dimx]) u.data();
-    float (*V)[dimy][dimx] = (float (*)[dimy][dimx]) v.data();
-    float (*W)[dimy][dimx] = (float (*)[dimy][dimx]) w.data();
+    float (*U)[dimy][dimz][0] = (float (*)[dimy][dimz]) _u.data();
+    float (*V)[dimy][dimz][0] = (float (*)[dimy][dimz]) _v.data();
+    float (*W)[dimy][dimz][0] = (float (*)[dimy][dimz]) _w.data();
 
-    float (*Txx)[dimy][dimx] = (float (*)[dimy][dimx]) txx.data();
-    float (*Tyy)[dimy][dimx] = (float (*)[dimy][dimx]) tyy.data();
-    float (*Tzz)[dimy][dimx] = (float (*)[dimy][dimx]) tzz.data();
-    float (*Tyz)[dimy][dimx] = (float (*)[dimy][dimx]) tyz.data();
-    float (*Txz)[dimy][dimx] = (float (*)[dimy][dimx]) txz.data();
-    float (*Txy)[dimy][dimx] = (float (*)[dimy][dimx]) txy.data();
+    float (*Txx)[dimy][dimz][0] = (float (*)[dimy][dimz]) _txx.data();
+    float (*Tyy)[dimy][dimz][0] = (float (*)[dimy][dimz]) _tyy.data();
+    float (*Tzz)[dimy][dimz][0] = (float (*)[dimy][dimz]) _tzz.data();
+    float (*Tyz)[dimy][dimz][0] = (float (*)[dimy][dimz]) _tyz.data();
+    float (*Txz)[dimy][dimz][0] = (float (*)[dimy][dimz]) _txz.data();
+    float (*Txy)[dimy][dimz][0] = (float (*)[dimy][dimz]) _txy.data();
 
-    float c0=9./8.;
-    float c1=1./24.;
+    for(int _ti=0;_ti<ntsteps;_ti++){
 
-    for(int ti=0;ti<ntsteps;ti++){
-      
+      int t = _ti % _tp; // array index of current time step
+      int t1 = _(t+1) % _tp // array index of the grid to be updated
+
       // Compute stresses
-#pragma omp for schedule(guided)
-      for(int k=2;k<dimz-2;k++){
-	for(int j=2;j<dimy-2;j++){
-	  for(int i=2;i<dimx-2;i++){
-	    ${Txx};
-	    ${Tyy};
-	    ${Tzz};
-	    ${Tyz};
-	    ${Txz};
-	    ${Txy};
-	  }
-	}
+    #pragma omp for schedule(guided)
+      for(int x=2;x<dimx-2;x++){
+      	for(int y=2;j<dimy-2;y++){
+      	  for(int z=2;i<dimz-2;z++){
+      	    ${Txx};
+      	    ${Tyy};
+      	    ${Tzz};
+      	    ${Tyz};
+      	    ${Txz};
+      	    ${Txy};
+      	  }
+      	}
       }
 
       // Compute velocities
-#pragma omp for schedule(guided)
+    #pragma omp for schedule(guided)
       for(int k=2;k<dimz-2;k++){
-	for(int j=2;j<dimy-2;j++){
-	  for(int i=2;i<dimx-2;i++){
-	    
-	    ${U};
-	    ${V};
-	    ${W};
-	  }
-	}
+        for(int j=2;j<dimy-2;j++){
+          for(int i=2;i<dimx-2;i++){
+            ${U};
+            ${V};
+            ${W};
+          }
+        }
       }
    
 #pragma omp single nowait
