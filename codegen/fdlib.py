c4de02acf68448f133bf766e3cf68c322d926f84
tj <ts2914@imperial.ac.uk>
2015-06-24 19:50:09 +0100

    update

--- codegen/fdlib.py
+++ codegen/fdlib.py
@@ -1,8 +1,12 @@
-from sympy import factorial, symbols, IndexedBase, solve, Matrix, Rational, Eq, simplify
+from sympy import factorial, symbols, Indexed, IndexedBase, solve, Matrix, Rational, Eq, simplify
 from sympy.printing.ccode import CCodePrinter
 from matplotlib import animation
 import numpy as np
 import matplotlib.pyplot as plt
+
+shift_x = ['U','Txy']
+shift_y = ['V','Txy']
+hf = Rational(1,2) # 1/2
 
 class MyCPrinter(CCodePrinter):
 	def __init__(self, settings={}):
@@ -82,7 +86,6 @@ def Deriv_half(U, i, k, d, n, shift_forward=True):
 	M = Taylor_half(d, n)
 	s = [0]*len(i)
 	s[k] = 1 # switch on kth dimension
-	hf = Rational(1,2) # 1/2
 	# generate matrix of RHS, i.e. [ ... U[x-1], U[x], U[x+1] ... ]
 	if len(i)==1:
 		RX = Matrix([U[i[0]+s[0]*x*hf] for x in range(-n*2+1,n*2,2)])
@@ -108,7 +111,6 @@ def Deriv_half_2(U, i, k, d, n):
 	M = Taylor_half(d, n)
 	s = [0]*len(i)
 	s[k] = 1 # switch on kth dimension
-	hf = Rational(1,2) # 1/2
 	# generate matrix of RHS, i.e. [ ... U[x-1], U[x], U[x+1] ... ]
 	if len(i)==1:
 		RX = Matrix([U[i[0]+s[0]*x*hf] for x in range(-n*2+1,n*2,2)])
@@ -125,7 +127,22 @@ def Deriv_half_2(U, i, k, d, n):
 	return result
 
 def shift_grid(expr):
-	return
+	if expr.is_Symbol:
+		return expr
+	if expr.is_Number:
+		return expr
+	if isinstance(expr,Indexed):
+		b = expr.base
+		idx = list(expr.indices)
+		if b.label.name in shift_x:
+			idx[1] -= hf
+		if b.label.name in shift_y:
+			idx[2] -= hf
+		t = Indexed(b,*idx)
+		return t
+	args = tuple([shift_grid(arg) for arg in expr.args])
+	expr2 = expr.func(*args)
+	return expr2
 
 def print_myccode(expr, assign_to=None, pochoir=False, **settings):
 	if pochoir:
