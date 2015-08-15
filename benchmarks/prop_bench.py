2865f3be6eade4c6d4f0fa087f6ffa468ece7d7d
Michael Lange <michael.lange@imperial.ac.uk>
2015-08-15 07:01:20 +0100

    Bench: Adding initial pybench setup to compare compiler settings
    This contains a compiler-and-run with a timer around the outer binary
    invocation and initial compiler settings for g++ and clang.
    
    Many thanks to Yukai Zhang for building the initial prototype of this.

---
+++ benchmarks/prop_bench.py
@@ -0,0 +1,67 @@
+import os
+import subprocess
+
+from pybench import Benchmark
+
+
+def switchcompiler(compiler, basename, opt_level):
+    """ Produce compiler command to run """
+    cc = 'g++'
+    cc += ' -fopenmp -O%d' % opt_level
+    cc += " -o %s_g++" % basename
+    cc += " %s.cpp" % basename
+
+    clang = 'clang++'
+    clang += ' -fopenmp'
+    clang += ' -o %s_clang' % basename
+    clang += ' %s.cpp'% basename
+    clang += ' -O%d' % opt_level
+
+    return {
+        'g++' : cc,
+        'clang' : clang,
+    }[compiler]
+
+class PropagatorBench(Benchmark):
+    """
+    Benchmarking tool for FD Propagator code
+
+    Execute benchmark runs with:
+    python prop_bench.py -b -s -l -- basename=<basename> compiler=<compiler> opt_level=<opt_level>
+    """
+    warmups = 0
+    repeats = 3
+
+    method = 'propagator'
+    benchmark = 'Propagator'
+
+    def compile(self, compiler, basename, opt_level):
+        cmd = switchcompiler(compiler, basename, opt_level)
+        try:
+            print "Compiling:", cmd
+            subprocess.check_call(cmd, shell=True)
+        except subprocess.CalledProcessError as e:
+            print "Compilation error: ", e
+            raise Exception("Failed to compile ")
+
+    def runlib(self, basename, compiler):
+        try:
+            cmd = os.getcwd() + "/" + basename + '_'+ compiler
+            print "Running:", cmd
+            with self.timed_region("running time with %s" % compiler):
+                # This is still rather crude since we're timing the OS
+                # and Python overheads of invoking the execution command
+                subprocess.check_call(cmd,shell=True)
+        except OSError as e:
+            print "running error:", e
+
+    def propagator(self, basename='test3d', compiler='g++', opt_level=3):
+        self.series['compiler'] = compiler
+        self.series['basename'] = basename
+        self.series['opt_level'] = opt_level
+
+        self.compile(compiler, basename, opt_level)
+        self.runlib(basename, compiler)
+
+
+PropagatorBench().main()
