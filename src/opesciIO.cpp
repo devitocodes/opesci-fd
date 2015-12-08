7298dde63a2938d56cbf58614c4bd3eab37d14b0
Gerard J Gorman <g.gorman@imperial.ac.uk>
2015-12-08 15:40:35 +0000

    Various fixes for segy reader.

--- src/opesciIO.cpp
+++ src/opesciIO.cpp
@@ -148,13 +148,16 @@ void opesci_dump_field_vts(std::string name, const int dims[], const float spaci
   
   {
     vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();  
+    points->SetNumberOfPoints(dims[0]*dims[1]*dims[2]);
+    vtkIdType pcnt=0;
     for(int k=0;k<dims[2];k++){
       float z = k*spacing[2];
       for(int j=0;j<dims[1];j++){
 	float y = j*spacing[1];
 	for(int i=0;i<dims[0];i++){
 	  float x = i*spacing[0];
-	  points->InsertNextPoint(x, y, z);
+	  points->SetPoint(pcnt, x, y, z);
+          pcnt++;
 	}
       }
     }
@@ -168,7 +171,7 @@ void opesci_dump_field_vts(std::string name, const int dims[], const float spaci
     for(int k=0;k<dims[2];k++){
       for(int j=0;j<dims[1];j++){
 	for(int i=0;i<dims[0];i++){
-	  int index = k*dims[0]*dims[1]+j*dims[0]+i;
+	  vtkIdType index = k*dims[0]*dims[1]+j*dims[0]+i;
 	  vtkfield->SetTuple1(index, field[index]);
 	}
       }
@@ -180,7 +183,7 @@ void opesci_dump_field_vts(std::string name, const int dims[], const float spaci
   writer->SetFileName(std::string(name+".vts").c_str());
   
   vtkSmartPointer<vtkZLibDataCompressor> compressor = vtkSmartPointer<vtkZLibDataCompressor>::New();
-  compressor->SetCompressionLevel(9);
+  compressor->SetCompressionLevel(1);
   writer->SetCompressor(compressor);
 
   // rewrite for vtk 6.20 -TJ
@@ -393,6 +396,57 @@ int opesci_read_receivers(const char *filename, std::vector<float> &array){
   return 0;
 }
 
+// Lifted from segy_util.c
+/* convert IBM FORTRAN REAL*4 to IEEE float */
+float real2float(const char *xreal)
+{
+  union
+  {
+    unsigned long l;
+    char c[4];
+  } yreal;
+  long vorzeichen, exponent, mantisse, i;
+  
+  for (i=0; i<4; i++)
+    yreal.c[i]=xreal[i];
+  vorzeichen=((yreal.l & 0x80000000) != 0);
+  exponent=((yreal.l & 0x7f000000) >> 24)-64;
+  mantisse=(yreal.l & 0x00ffffff);
+  if (vorzeichen)
+    return(-mantisse/16777216.0*pow(16.0,(double)exponent));
+  else
+    return( mantisse/16777216.0*pow(16.0,(double)exponent));
+}
+
+// Unpack required data from header. Start by assuming endian swap is not required.
+inline int16_t unpack_int16(const char *target, bool swap_endian){
+  if(swap_endian){
+    char buffer[2] = {target[1], target[0]};
+    return *(int16_t *)(buffer);
+  }else{
+    return *(int16_t *)(target);
+  }
+}
+
+inline int32_t unpack_int32(const char *target, bool swap_endian){
+  if(swap_endian){
+    char buffer[4] = {target[3], target[2], target[1], target[0]};
+    return *(int32_t *)(buffer);
+  }else{
+    return *(int32_t *)(target);
+  }
+}
+
+inline float unpack_ibmfloat(const char *target, bool swap_endian){
+  if(swap_endian){
+    char buffer[4] = {target[3], target[2], target[1], target[0]};
+    return real2float(buffer);
+  }else{
+    return real2float(target);
+  }
+}
+
+// Assuming http://www.seg.org/documents/10161/77915/seg_y_rev1.pdf
 int opesci_read_model_segy(const char *filename, std::vector<float> &array, int dim[], float spacing[]){
   std::ifstream infile(filename);
   if(!infile.good()){
@@ -403,55 +457,170 @@ int opesci_read_model_segy(const char *filename, std::vector<float> &array, int 
   // Get the size of the file.
   infile.seekg (0, infile.end);
   long filesize = infile.tellg();
- 
-  // Get the number of depth layers.
-  infile.seekg(3220, std::ios::beg);
-  char ns[2];
-  infile.read(ns, 2);
-  dim[0] = *(int16_t *)(ns);
+
+  // Read in header
+  char header_buffer[3600];
+  infile.seekg(0, infile.beg);
+  infile.read(header_buffer, 3600);
   
-  // Calculate number of traces in this file and the other dimensions
-  // of the dataset. Include a heuristic for checking endian.
   bool swap_endian=false;
-  int tracesize = 240+4*dim[0];
-  int ntraces = (filesize-3600)/tracesize + 0.5;
-  dim[1] = (int)(sqrt(ntraces)+0.5);
-  dim[2] = dim[1];
-  if(dim[1]*dim[2]!=ntraces){
-    swap_endian=true;
-    std::swap(ns[0], ns[1]);
-    dim[0] = *(int16_t *)(ns);
+  int ntraces, tracesize, format_code;
+  for(int i=0;i<2;i++){ // Enables a re-try of header read.
+    // 3201 - 3204 	Job identification number.
+    // std::cout<<"3201 - 3204 	Job identification number: "<<unpack_int32(header_buffer+3200, swap_endian)<<std::endl;
+
+    // 3205 - 3208 	Line number.
+    // std::cout<<"3205 - 3208 	Line number: "<<unpack_int32(header_buffer+3204, swap_endian)<<std::endl;
+
+    // 3209 - 3212 	Reel number.
+    // std::cout<<"3209 - 3212 	Reel number: "<<unpack_int32(header_buffer+3208, swap_endian)<<std::endl;
+
+    // 3213 - 3214 	Number of data traces per record.
+    dim[2] = unpack_int16(header_buffer+3212, swap_endian);
     
+    // 3215 - 3216 	Number of auxiliary traces per record.
+    // std::cout<<"3215 - 3216 	Number of auxiliary traces per record: "<<unpack_int16(header_buffer+3214, swap_endian)<<std::endl;
+
+    // 3217 - 3218 	Sample interval, microseconds, this file (reel).
+    // std::cout<<"3217 - 3218 	Sample interval, microseconds, this file (reel): "<<unpack_int16(header_buffer+3216, swap_endian)<<std::endl;
+
+    // 3219 - 3220 	Sample interval, microseconds, original field recording.
+    // 3221 - 3222 	Number of samples per data trace, this file (reel).
+    dim[0] = unpack_int16(header_buffer+3220, swap_endian);
+    
+    // 3223 - 3224 	Number of samples per data trace, original field recording.
+    // 3225 - 3226 	Data sample format code: 1 = 4-byte IBM floating-point
+    //                                           2 = 4-byte, two's complement integer
+    //                                           3 = 2-byte, two's complement integer
+    //                                           4 = 4-byte fixed-point with gain (obsolete)
+    //                                           5 = 4-byte IEEE floating-point
+    //                                           6 = Not currently used
+    //                                           7 = Not currently used
+    //                                           8 = 1-byte, two's complement integer
+    format_code = unpack_int16(header_buffer+3224, swap_endian);
+    if(format_code<1 || format_code>8){
+      swap_endian = true;
+      format_code = unpack_int16(header_buffer+3224, swap_endian);
+      
+      // Try again...
+      if(format_code<1 || format_code>8){
+	std::cerr<<"ERROR: unsupported data sample format code "<<format_code<<std::endl;
+	return -1;
+      }
+      
+      continue; // restart header read
+    }
+    
+    // 3227 - 3228 	CDP fold.
+    // std::cout<<"3227 - 3228 	CDP fold: "<<unpack_int16(header_buffer+3226, swap_endian)<<std::endl;
+
+    // 3229 - 3230 	Trace sorting code: Trace sorting code (i.e. type of ensemble) :
+    //                                      -1 = Other (should be explained in user Extended Textual File Header stanza
+    //                                       0 = Unknown
+    //                                       1 = As recorded (no sorting)
+    //                                       2 = CDP ensemble
+    //                                       3 = Single fold continuous profile
+    //                                       4 = Horizontally stacked
+    //                                       5 = Common source point
+    //                                       6 = Common receiver point
+    //                                       7 = Common offset point
+    //                                       8 = Common mid-point
+    //                                       9 = Common conversion point
+    // std::cout<<"3229 - 3230 	Trace sorting code: "<<unpack_int16(header_buffer+2328, swap_endian)<<std::endl;
+
+    // 3231 - 3232 	Vertical sum code: 1 = no sum 2 = two sum ... N = N sum (N = 32,767)
+    // std::cout<<"3231 - 3232 	Vertical sum code: "<<unpack_int16(header_buffer+2330, swap_endian)<<std::endl;
+
+    // 3233 - 3234 	Sweep frequency at start.
+    // 3235 - 3236 	Sweep frequency at end.
+    // 3237 - 3238 	Sweep length, ms.
+    // 3239 - 3240 	Sweep type code: 1 = linear 
+    //                                   2 = parabolic 
+    //                                   3 = exponential 
+    //                                   4 = other
+    // 3241 - 3242 	Trace number of sweep channel.
+    // std::cout<<"3241 - 3242 	Trace number of sweep channel: "<<unpack_int16(header_buffer+2340, swap_endian)<<std::endl;
+
+    // 3243 - 3244 	Sweep trace taper length, ms, at start if tapered.
+    // std::cout<<"3243 - 3244 	Sweep trace taper length, ms, at start if tapered: "<<unpack_int16(header_buffer+2342, swap_endian)<<std::endl;
+
+    // 3245 - 3246 	Sweep trace taper length, ms, at end.
+    // std::cout<<"3245 - 3246 	Sweep trace taper length, ms, at end: "<<unpack_int16(header_buffer+2344, swap_endian)<<std::endl;
+
+    // 3247 - 3248 	Taper type: 1 = linear 2 = cos 3 = other
+    // std::cout<<"3247 - 3248 	Taper type: 1 = linear 2 = cos 3 = other: "<<unpack_int16(header_buffer+2346, swap_endian)<<std::endl;
+
+    // 3249 - 3250 	Correlated data traces: 1 = no 2 = yes
+    // std::cout<<"3249 - 3250 	Correlated data traces: 1 = no 2 = yes"<<unpack_int16(header_buffer+2348, swap_endian)<<std::endl;
+
+    // 3251 - 3252 	Binary gain recovered: 1 = yes 2 = no
+    // std::cout<<"3251 - 3252 	Binary gain recovered: 1 = yes 2 = no: "<<unpack_int16(header_buffer+2350, swap_endian)<<std::endl;
+
+    // 3253 - 3254 	Amplitude recovery method: 1 = none 2 = spherical divergence 3 = AGC 4 = other
+    // std::cout<<"3253 - 3254 	Amplitude recovery method: 1 = none 2 = spherical divergence 3 = AGC 4 = other: "<<unpack_int16(header_buffer+2352, swap_endian)<<std::endl;
+
+    // 3255 - 3256 	Measurement system: 1 = meters 2 = feet
+    // std::cout<<"3255 - 3256 	Measurement system: 1 = meters 2 = feet: "<<unpack_int16(header_buffer+2354, swap_endian)<<std::endl;
+
+    // 3257 - 3258 	Impulse signal: 1 = Upward = negative number. 
+    //                                  2 = Upward = positive number.
+    // std::cout<<"3257 - 3258 	Impulse signal: "<<unpack_int16(header_buffer+2356, swap_endian)<<std::endl;
+
+    // 3259 - 3260 	Vibratory polarity code - seismic signal lags pilot signal by: 1 = 337.5 - 22.5 degrees 
+    //                                                                                 2 = 22.5 - 67.5 degrees 
+    //                                                                                 3 = 67.5 - 112.5 degrees 
+    //                                                                                 4 = 112.5 - 157.5 degrees 
+    //                                                                                 5 = 157.5 - 202.5 degrees 
+    //                                                                                 6 = 202.5 - 247.5 degrees
+    // std::cout<<"3259 - 3260 	Vibratory polarity code - seismic signal lags pilot signal by: "<<unpack_int16(header_buffer+2358, swap_endian)<<std::endl;
+    
+    // Set tracesize
     tracesize = 240+4*dim[0];
-    ntraces = (filesize-3600)/tracesize + 0.5;
-    dim[1] = (int)(sqrt(ntraces)+0.5);
-    dim[2] = dim[1];
+    
+    // Get number of traces.
+    ntraces = (filesize-3600)/tracesize;
+
+    dim[1] = ntraces/dim[2];
+    
+    break;
   }
 
   array.resize(dim[0]*dim[1]*dim[2]);  
-  if(swap_endian){
-    char buffer[dim[0]*4];
+  if(format_code==1){
+    char trace_buffer[tracesize];
+    float x0[2], x1[2], scale_xy;
     for(int i=0;i<ntraces;i++){
-      infile.seekg(3600+i*tracesize+240, std::ios::beg);
-      infile.read(buffer, dim[0]*4);
-      
-      for(int j=0;j<dim[0];j++){
-	std::swap(buffer[j*4], buffer[j*4+3]);
-	std::swap(buffer[j*4+1], buffer[j*4+2]);
+      infile.read(trace_buffer, tracesize);
+
+      if(i==0){
+	scale_xy = unpack_int16(trace_buffer+70, swap_endian);
+	if(scale_xy<0)
+	  scale_xy = 1.0/fabs(scale_xy);
+	x0[0] = scale_xy*unpack_int32(trace_buffer+72, swap_endian);
+	x0[1] = scale_xy*unpack_int32(trace_buffer+76, swap_endian);
+      }else if(i==1){
+	x1[0] = scale_xy*unpack_int32(trace_buffer+72, swap_endian);
+	x1[1] = scale_xy*unpack_int32(trace_buffer+76, swap_endian);
+
+	float dx = std::max(fabs(x0[0]-x1[0]), fabs(x0[0]-x1[0]));
+	
+	spacing[0] = dx;
+	spacing[1] = dx;
+	spacing[2] = dx;
       }
-      memcpy(array.data()+i*dim[0], buffer, dim[0]*4);
+
+      for(int j=0;j<dim[0];j++){
+	char *next = trace_buffer+240+j*4;
+	array[i*dim[0]+j] = unpack_ibmfloat(next, swap_endian);
+      }
     }
   }else{
-    for(int i=0;i<ntraces;i++){
-      infile.seekg(3600+i*tracesize+240, std::ios::beg);
-      infile.read((char *)(array.data()+i*dim[0]), dim[0]*4);
-    }
+    std::cerr<<"ERROR: format code "<<format_code<<" not yet supported"<<std::endl;
+    return -1;
   }
 
   return 0;
 }
-
-
 
 void opesci_dump_field_vts_3d(std::string name, const int dims[], const float spacing[], int margin, float *field){
 #ifdef VTK_FOUND
